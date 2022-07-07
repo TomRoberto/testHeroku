16f796b739427efbf3eb1495527adc5f78cb301e
Tom ROBERTO <tom.roberto31@gmail.com>
2022-07-07 14:17:01 +0200

    fkdzaenjkflazejh

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.all("*", (req, res) => {
+  res.json("ckldenljfczbeal");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("fzelkfnljzefj");
+});
